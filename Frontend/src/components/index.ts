5c8daf9778c21e07afb413085741025fa4bf29a4
Oman <omanapple42@Hotmail.com>
2023-01-12 12:41:52 -0300

    Add files via upload

---
+++ Frontend/src/components/index.ts
@@ -0,0 +1,21 @@
+import Header from "./Header";
+import Product from "./Product";
+import Footer from "./Footer";
+import Rating from "./Rating";
+import LoadingBox from "./LoadingBox";
+import MessageBox from "./MessageBox";
+import CheckoutSteps from "./CheckoutSteps";
+import ProtectedRoute from "./ProtecetedRoute";
+import AdminRoute from "./AdminRoute";
+
+export { 
+  Header, 
+  Footer, 
+  Product, 
+  Rating,
+  LoadingBox,
+  MessageBox,
+  CheckoutSteps,
+  ProtectedRoute,
+  AdminRoute
+}
